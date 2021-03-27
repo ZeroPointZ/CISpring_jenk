c7dccc681b7663f30c2a343ee4ed0ef28563d2a2
student <mikedorado@gmail.com>
2021-03-27 17:38:22 +0000

    jenkins file demo

---
+++ src/main/java/com/example/CISpring_jenk/CiSpringJenkApplication.java
@@ -0,0 +1,13 @@
+package com.example.CISpring_jenk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiSpringJenkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiSpringJenkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CISpring_jenk/CiSpringJenkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CISpring_jenk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiSpringJenkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
